24117d7abb91611475ccec8b6bb3d4ae3e6f4db9
YMH <asds2534@naver.com>
2025-03-10 17:54:06 +0900

    나머지  문제

--- exemple.c
+++ exemple.c
@@ -3,10 +3,13 @@
 
 int main(void)
 {
-	int a;
+	int a, b, c;
 
-	scanf("%d", &a);
-	printf("%d", a - 543);
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%d\n", (a + b) % c);
+	printf("%d\n", ((a % c) + (b % c)) % c);
+	printf("%d\n", (a * b) % c);
+	printf("%d\n", ((a % c) * (b % c)) % c);
+
 	return 0;
-
 }
