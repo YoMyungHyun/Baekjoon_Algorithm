f96856e5d948ebae60628e93777d2e04c396f7d9
YMH <asds2534@naver.com>
2025-03-12 17:59:09 +0900

    A + B -4 문제 주석 추가

--- Algorithm.c
+++ Algorithm.c
@@ -3,8 +3,8 @@
 
 
 int main(void) {
-    int a, b;
-    while(scanf("%d %d", &a, &b) != EOF) {
+    int a, b;                              // 테스트 케이스의 개수를 미리 주어주지 않은 문제
+    while(scanf("%d %d", &a, &b) != EOF) { // EOF 즉, 끝나는 시점이면 while문을 빠져나와라
         printf("%d\n", a+b);
     }
     return 0;
