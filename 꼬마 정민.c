7d6e4896a69f9128836d00bbd83f95581b344268
YMH <asds2534@naver.com>
2025-03-18 23:57:33 +0900

    오븐 시계 문제

--- 꼬마 정민.c
+++ 꼬마 정민.c
@@ -1,13 +1,13 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-int main(void)
-{
-	long long int a, b, c;
-
-	scanf("%lld %lld %lld", &a, &b, &c);
-	printf("%lld", a + b + c);
-
-	return 0;
-
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	long long int a, b, c;
+//
+//	scanf("%lld %lld %lld", &a, &b, &c);
+//	printf("%lld", a + b + c);
+//
+//	return 0;
+//
+//}
