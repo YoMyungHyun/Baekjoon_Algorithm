74be2248c5779aba3ee9eaa6ca4b5a55efa795ab
YMH <asds2534@naver.com>
2025-03-19 13:48:41 +0900

    설탕 배달 문제

--- 오븐 시계.c
+++ 오븐 시계.c
@@ -1,22 +1,22 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-int main(void) {
-	int A, B, C;
-
-	scanf("%d %d %d", &A, &B, &C);
-
-	B = B + C;
-
-	if (B > 59) {
-		A = A + B / 60; // B+C가 60을 넘었으면 60으로 나눈 값을 A에 더해줌
-		B = B % 60; // B+C가 60을 넘었으면 %로 나머지 값을 구해줌 ex) 61 % 60 = 1분
-	}
-	if (A >= 24) {
-		A = A % 24; // A의 값이 24를 넘었으면 %로 나머지 값을 구해줌 ex) 25 % 24 = 1
-	}
-
-	printf("%d %d", A, B);
-	return 0;
-
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main(void) {
+//	int A, B, C;
+//
+//	scanf("%d %d %d", &A, &B, &C);
+//
+//	B = B + C;
+//
+//	if (B > 59) {
+//		A = A + B / 60; // B+C가 60을 넘었으면 60으로 나눈 값을 A에 더해줌
+//		B = B % 60; // B+C가 60을 넘었으면 %로 나머지 값을 구해줌 ex) 61 % 60 = 1분
+//	}
+//	if (A >= 24) {
+//		A = A % 24; // A의 값이 24를 넘었으면 %로 나머지 값을 구해줌 ex) 25 % 24 = 1
+//	}
+//
+//	printf("%d %d", A, B);
+//	return 0;
+//
+//}
